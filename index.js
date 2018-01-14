7174c0674dd00c32d6b5cfb297f49897d1f906f8
Emily Brumfield <belac54@hotmail.com>
2018-01-14 05:42:51 -0500

    add service worker registration

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+//service worker registration if browser supports it
+//an actual service worker might be overkill for an app this simple, but it's good to get practice
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+  .then(function(registration) {
+    console.log('Registration successful, scope is:', registration.scope);
+  })
+  .catch(function(error) {
+    console.log('Service worker registration failed, error:', error);
+  });
+}
+
 /*SHADOWRUN ANARCHY DICE ROLLER
 Progressiva web app that rolls Shadowrun Anarchy dice without hassle or unnecessary clicks.
 
